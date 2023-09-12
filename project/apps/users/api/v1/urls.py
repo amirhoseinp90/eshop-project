5a58cbb23fa5d83279a757cb5a7f7e1c7a0e87d1
amirhosein <amirhoseinp90@gmail.com>
2023-09-12 22:44:31 +0330

    feat(url) Add create user endpoint
    This commit introduce create user path to 'urls.py'

--- project/apps/users/api/v1/urls.py
+++ project/apps/users/api/v1/urls.py
@@ -1,10 +1,12 @@
 """
 Users API version 1 endpoints.
 """
-from django.urls import path
+from django.urls import path, include
+
+from project.apps.users.api.v1 import views
 
 
 app_name = 'v1'
 urlpatterns = [
-
+    path('create-user/', views.CreateUserView.as_view(), name='create-user')
 ]
