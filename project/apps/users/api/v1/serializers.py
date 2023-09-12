92b63d3c34094e416d5ad3b91fb104b8c3863ead
amirhosein <amirhoseinp90@gmail.com>
2023-09-12 22:28:34 +0330

    feat(serializer) Add create user serializer
    This commit introduce create user serializer in the 'users' app.
    the serializer is designed to hande creating user  and  includes
    fields for 'username', 'email' and 'password'. and custom 'create'
    method to create user with encrypted password.

--- project/apps/users/api/v1/serializers.py
+++ project/apps/users/api/v1/serializers.py
@@ -1,3 +1,21 @@
 """
 Users version 1 serializers.
 """
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+from project.apps.users.models import User
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    """Serializer for creating user."""
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create and return a user with encrypted password."""
+        return get_user_model().objects.create_user(**validated_data)
