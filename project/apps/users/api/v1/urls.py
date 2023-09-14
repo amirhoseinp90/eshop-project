4e53d4eab0308809c83f2edf8c5011ab2c6e9fb8
amirhosein <amirhoseinp90@gmail.com>
2023-09-14 12:21:24 +0330

    feat(url) Add 'create token' endpoint
    This commit introduces a new 'create token' endpoint in
    'urls.py' to enable token creation for user authentication.

--- project/apps/users/api/v1/urls.py
+++ project/apps/users/api/v1/urls.py
@@ -8,5 +8,6 @@ from project.apps.users.api.v1 import views
 
 app_name = 'v1'
 urlpatterns = [
-    path('create-user/', views.CreateUserView.as_view(), name='create-user')
+    path('create-user/', views.CreateUserView.as_view(), name='create-user'),
+    path('create-token', views.CreatTokenView.as_view(), name='create-token')
 ]
