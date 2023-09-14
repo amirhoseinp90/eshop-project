7dea68131c36d8a98c03efaba874256f683c4cda
amirhosein <amirhoseinp90@gmail.com>
2023-09-14 12:14:41 +0330

    feat(view) Add custom create token views
    This commit adds custom create token views to
    include custom create token serializer.

--- project/apps/users/api/v1/views.py
+++ project/apps/users/api/v1/views.py
@@ -4,12 +4,18 @@ Views for user API endpoints.
 # from rest_framework.viewsets import GenericViewSet
 from rest_framework.generics import CreateAPIView
 from rest_framework.mixins import CreateModelMixin
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from project.apps.users.api.base.permissions import UnauthenticatedOnlyPermission
-from project.apps.users.api.v1.serializers import CreateUserSerializer
+from project.apps.users.api.v1.serializers import CreateUserSerializer, CreateTokenSerializer
 
 
 class CreateUserView(CreateAPIView):
     """Create a new user in the system."""
     serializer_class = CreateUserSerializer
     permission_classes = [UnauthenticatedOnlyPermission]
+
+
+class CreatTokenView(ObtainAuthToken):
+    """Create token for the user."""
+    serializer_class = CreateTokenSerializer
